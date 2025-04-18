6321c497076c18b331bee3424fc946170e813858
gdziewon <e.dziewonski@gmail.com>
2025-04-18 20:46:37 +0200

    Implement twitter scraper

---
+++ config.py
@@ -0,0 +1,50 @@
+CHROME_ARGS = [
+    "--profile-directory=CustomerProfile",
+    "--disable-features=SameSiteByDefaultCookies,CookiesWithoutSameSiteMustBeSecure",
+    "--disable-blink-features=AutomationControlled",
+    "--force-device-scale-factor=1",
+    "--lang=pl-PL",
+    "--disable-web-security",
+    "--use-fake-ui-for-media-stream",
+    "--use-fake-device-for-media-stream",
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+    "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
+    "--disable-gpu",
+    "--disable-webgl",
+    "--disable-webrtc",
+    "--disable-features=NetworkService,NetworkServiceInProcess",
+]
+
+COOKIES = [
+    {
+      "name": "is_user_logged_in",
+      "value": "false",
+      "domain": "www.x-kom.pl",
+      "path": "/",
+      "expires": -1,
+      "httpOnly": False,
+      "secure": False,
+      "sameSite": "Lax"
+    },
+    {
+      "name": "trackingPermissionConsentsValue",
+      "value": "%7B%22cookies_analytics%22%3Atrue%2C%22cookies_personalization%22%3Atrue%2C%22cookies_advertisement%22%3Atrue%7D",
+      "domain": "www.x-kom.pl",
+      "path": "/",
+      "expires": 1772412318.075002,
+      "httpOnly": False,
+      "secure": False,
+      "sameSite": "Lax"
+    },
+    {
+      "name": "test_cookie",
+      "value": "CheckForPermission",
+      "domain": ".doubleclick.net",
+      "path": "/",
+      "expires": 1740856269.305768,
+      "httpOnly": False,
+      "secure": True,
+      "sameSite": "None"
+    },
+  ]
---
+++ twitter.py
@@ -0,0 +1,102 @@
+from patchright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
+import re
+import json
+import random
+from pathlib import Path
+import time
+from config import COOKIES, CHROME_ARGS
+from urllib.parse import urljoin
+
+def save_data(keyword, results):
+    output_dir = Path("output")
+    output_dir.mkdir(exist_ok=True)
+    
+    json_path = output_dir / f"{keyword}_tweets.json"
+    with open(json_path, "w", encoding="utf-8") as f:
+        json.dump(results, f, indent=2, ensure_ascii=False)
+
+# check if the tweet contains the keyword in the text
+def should_keep_tweet(tweet_element, keyword):
+    text_div = tweet_element.find("div", {"data-testid": "tweetText"})
+    if not text_div:
+        return False
+        
+    tweet_text = ' '.join([span.text for span in text_div.find_all("span")])
+    return re.search(rf'\b{re.escape(keyword)}\b', tweet_text, re.IGNORECASE)
+
+def extract_tweet_data(article):
+    tweet_data = {
+        "url": None,
+        "username": None,
+        "text": None,
+        "date": None
+    }
+
+    link = article.find("a", {"href": re.compile(r'/status/')})
+    if link:
+        tweet_data["url"] = urljoin("https://x.com", link["href"])
+
+    user_section = article.find("div", {"data-testid": "User-Name"})
+    if user_section:
+        username_link = user_section.find("a", href=re.compile(r'^/'))
+        if username_link:
+            tweet_data["username"] = username_link.text.strip('@')
+
+    text_div = article.find("div", {"data-testid": "tweetText"})
+    if text_div:
+        tweet_data["text"] = ' '.join([span.text for span in text_div.find_all("span")])
+
+    time_tag = article.find("time")
+    if time_tag and "datetime" in time_tag.attrs:
+        tweet_data["date"] = time_tag["datetime"]
+
+    return tweet_data
+
+def scrape_tweets(keyword, tweet_num):
+    link = f"https://x.com/search?q={keyword}&f=live&src=typed_query"
+
+    with sync_playwright() as p:
+        browser = p.chromium.launch_persistent_context(
+            "states/twitter_context",
+            headless=False,
+            args=CHROME_ARGS,
+        )
+        browser.add_cookies(COOKIES)
+        page = browser.new_page()
+        page.goto(link)
+        page.wait_for_selector("article", timeout=15000)
+
+        seen_urls = set()
+        results = [] 
+        last_html = ""
+        
+        while len(results) < tweet_num:
+            page.mouse.wheel(
+                    0, 
+                    random.randint(8000, 12000) + 
+                    random.gauss(500, 200)
+                )
+            time.sleep(random.uniform(1, 2))
+
+            current_html = page.content()
+            if current_html == last_html:
+                continue
+
+            soup = BeautifulSoup(current_html, "html.parser")
+            articles = soup.find_all("article")
+            
+            for article in articles:
+                if not should_keep_tweet(article, keyword):
+                    continue
+                    
+                tweet_data = extract_tweet_data(article)
+                
+                if tweet_data["url"] and tweet_data["url"] not in seen_urls:
+                    seen_urls.add(tweet_data["url"])
+                    results.append(tweet_data)
+                    print(f"Collected {len(results)}/{tweet_num}")
+            last_html = current_html
+        
+    save_data(keyword, results[:tweet_num])
+    return results
